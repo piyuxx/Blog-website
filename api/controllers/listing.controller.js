a97adc954869c8fb0ac9d7a7ae7c26c3393ae3c6
piyuxx <piyugautam2366@gmail.com>
2024-05-30 03:44:30 +0530

    changes

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -87,7 +87,7 @@ export const getListings = async (req, res, next) => {
         }).sort({ [sort]: order })
             .limit(limit)
             .skip(startIndex)
-        return res.status(200).length(length).json(listings);
+        return res.status(200).json(listings);
 
     } catch (error) {
         next(error)
